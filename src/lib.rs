04d689ba63fac4e821f0f2adb3f7245d7803fe26
junzezhu <junzezhu1999@gmail.com>
2023-06-11 00:06:08 -0700

    add highlight

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ impl Config {
             Some(q) => q,
             None => return Err("No filepath specified!"),
         };
+
         Ok(Config { query: query, filepath: filepath,ignore_case:env::var("IGNORE_CASE").is_ok() })
     }
 }
@@ -34,8 +35,8 @@ pub fn run(config:Config)->Result<(),Box<dyn Error>>{
     }
     Ok(())
 }
-pub fn search<'a>(query:& str,content:&'a str)->Vec<&'a str>{
-    // let mut ans=Vec::new();
+pub fn search<'a>(query:& str,content:&'a str)->Vec<String>{
+    let mut ans=Vec::new();
     // for line in content.lines(){
     //     if line.contains(query)
     //     {
@@ -43,11 +44,21 @@ pub fn search<'a>(query:& str,content:&'a str)->Vec<&'a str>{
     //     }
     // }
     // ans
-    content.lines().filter(|line| line.contains(query)).collect()
+    let contains:Vec<&str>=content.lines().filter(|line| line.contains(query)).collect();
+    for l in contains{
+        let matches:Vec<_>=l.rmatch_indices(query).collect();
+        let mut temp=String::from(l);
+        for m in matches{
+            temp.insert_str(m.0+query.len(), "\x1B[0m");
+            temp.insert_str(m.0, "\x1B[7m");
+        }
+        ans.push(temp);
+    }
+    return ans;
 }
 
-pub fn search_case_insensitive<'a>(query:& str,content:&'a str)->Vec<&'a str>{
-    // let mut ans=Vec::new();
+pub fn search_case_insensitive<'a>(query:& str,content:&'a str)->Vec<String>{
+    let mut ans: Vec<_>=Vec::new();
     // let query=query.to_lowercase();
     // for line in content.lines(){
     //     if line.to_lowercase().contains(&query)
@@ -56,7 +67,18 @@ pub fn search_case_insensitive<'a>(query:& str,content:&'a str)->Vec<&'a str>{
     //     }
     // }
     // ans
-    content.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
+    let contains:Vec<&str>=content.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect();
+    for l in contains{
+        let temp_l=l.to_lowercase();
+        let matches:Vec<_>=temp_l.rmatch_indices(&query.to_lowercase()).collect();
+        let mut temp=String::from(l);
+        for m in matches{
+            temp.insert_str(m.0+query.len(), "\x1B[0m");
+            temp.insert_str(m.0, "\x1B[7m");
+        }
+        ans.push(temp);
+    }
+    ans
 }
 
 #[cfg(test)]
